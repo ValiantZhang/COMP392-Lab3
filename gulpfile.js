8749b01e21b0c717d23701fa47bd83d69d2bc536
Jacky Zhang <jacky_02468@hotmail.com>
2016-02-08 22:34:05 +0000

    config folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 //adding required modules
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var debug = require('gulp-debug');
+var inject = require('gulp-inject');
+var tsc = require('gulp-typescript');
+var sourcemaps = require('gulp-sourcemaps');
+var tscProject = tsc.createProject('tsconfig.json');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
 
 //example task
 {
