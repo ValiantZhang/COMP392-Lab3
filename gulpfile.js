aa1e3205d4ce25727468eefa3c295a0e8f7f9738
Jacky Zhang <jacky_02468@hotmail.com>
2016-02-08 22:29:09 +0000

    gitignore

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+//adding required modules
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+//example task
+{
+    gulp.task('example', function() {
+        gutil.log("example started...");
+    });
+}
